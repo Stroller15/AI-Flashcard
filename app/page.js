17681b8310179bc6ce98715809f6405c887e2be0
ShobyG <shobysekar@gmail.com>
2024-08-20 14:51:56 -0700

    landing page overflow

--- app/page.js
+++ app/page.js
@@ -1,12 +1,6 @@
 "use client";
 
-import {
-  Container,
-  Box,
-  Typography,
-  Button,
-  Grid,
-} from "@mui/material";
+import { Container, Box, Typography, Button, Grid } from "@mui/material";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import getStripe from "@/utils/get-stripe";
 import CustomAppBar from "./CustomAppBar";
@@ -36,7 +30,12 @@ export default function Home() {
     );
   };
   return (
-    <Container maxWidth="100%" fixed bgcolor="#f2f6fc" sx={{height: "100vh"}}>
+    <Container
+      maxWidth="100%"
+      fixed
+      bgcolor="#f2f6fc"
+      sx={{ height: "100vh", overflow: "auto" }}
+    >
       <CustomAppBar />
       <Box sx={{ textAlign: "center", my: 4 }}>
         <Typography variant="h2" component="h1" gutterBottom>
@@ -94,42 +93,75 @@ export default function Home() {
             Login for Saved Flashcards
           </Button>
           <Box sx={{ my: 6 }}>
-          <Typography variant="h4" component="h2" gutterBottom>
-            Features
-          </Typography>
-          <Grid container spacing={4} justifyContent="center">
-            <Grid item xs={12} md={4}>
-              <Typography variant="h6">Effortless Content Creation</Typography>
-              <Typography>{' '} Transform your notes into study material effortlessly. Just paste your text, and our system will convert it into ready-to-use flashcards.</Typography>
+            <Typography variant="h4" component="h2" gutterBottom>
+              Features
+            </Typography>
+            <Grid container spacing={4} justifyContent="center">
+              <Grid item xs={12} md={4}>
+                <Typography variant="h6">
+                  Effortless Content Creation
+                </Typography>
+                <Typography>
+                  {" "}
+                  Transform your notes into study material effortlessly. Just
+                  paste your text, and our system will convert it into
+                  ready-to-use flashcards.
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <Typography variant="h6">Intelligent Study Aids</Typography>
+                <Typography>
+                  {" "}
+                  Our AI optimizes your study sessions by generating flashcards
+                  that highlight key concepts, helping you retain information
+                  more effectively.
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <Typography variant="h6">Universal Access</Typography>
+                <Typography>
+                  {" "}
+                  Enjoy seamless studying across all your devices, whenever and
+                  wherever you need. Your flashcards are always within reach,
+                  ensuring you can study anytime.
+                </Typography>
+              </Grid>
             </Grid>
-            <Grid item xs={12} md={4}>
-              <Typography variant="h6">Intelligent Study Aids</Typography>
-              <Typography>{' '} Our AI optimizes your study sessions by generating flashcards that highlight key concepts, helping you retain information more effectively.</Typography>
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <Typography variant="h6">Universal Access</Typography>
-              <Typography>{' '} Enjoy seamless studying across all your devices, whenever and wherever you need. Your flashcards are always within reach, ensuring you can study anytime.</Typography>
-            </Grid>
-          </Grid>
-        </Box>
+          </Box>
           <Box sx={{ my: 6, textAlign: "center" }}>
             <Typography variant="h4" component="h2" gutterBottom>
               Pricing
             </Typography>
             <Grid container spacing={4} justifyContent="center">
               <Grid item xs={12} md={4}>
-                <Box sx={{p:3, border: '1px solid', borderColor: 'greay.300', borderRadius:2,}}>
-                  <Typography variant="h5" gutterBottom>Pro</Typography>
-                  <Typography variant="h6" gutterBottom>$5 / month</Typography>
-                  <Typography>{' '} Access to Pro flashcard features and storage.</Typography>
-                  <Button variant="contained" sx={{
-                    mt: 2,
-                    mr: 2,
-                    backgroundColor: "#BC8F8F",
-                    color: "#FFFFFF",
-                    "&:hover": {
-                      backgroundColor: "#A77D7D",
-                    },
+                <Box
+                  sx={{
+                    p: 3,
+                    border: "1px solid",
+                    borderColor: "greay.300",
+                    borderRadius: 2,
+                  }}
+                >
+                  <Typography variant="h5" gutterBottom>
+                    Pro
+                  </Typography>
+                  <Typography variant="h6" gutterBottom>
+                    $5 / month
+                  </Typography>
+                  <Typography>
+                    {" "}
+                    Access to Pro flashcard features and storage.
+                  </Typography>
+                  <Button
+                    variant="contained"
+                    sx={{
+                      mt: 2,
+                      mr: 2,
+                      backgroundColor: "#BC8F8F",
+                      color: "#FFFFFF",
+                      "&:hover": {
+                        backgroundColor: "#A77D7D",
+                      },
                     }}
                     onClick={handleSubmit}
                   >
